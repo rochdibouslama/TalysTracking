f4a42e5b9bee877a914e62e1b53530975b349875
rochdibouslama <rochdi.bouslama@esprit.tn>
2019-07-29 11:36:26 +0100

    First commit

---
+++ src/main/java/com/talys_tracking/TalysTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.talys_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalysTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalysTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talys_tracking/TalysTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talys_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalysTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
